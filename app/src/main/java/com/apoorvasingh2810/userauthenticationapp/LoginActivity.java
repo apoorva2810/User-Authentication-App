b9b1e7d9a2a331578428e45d7233faf3ef796598
Apoorva Singh <apoorva.singh2810@gmail.com>
2018-07-24 03:44:42 +0530

    Login Activity - completed and Profile Activity- created

--- app/src/main/java/com/apoorvasingh2810/userauthenticationapp/LoginActivity.java
+++ app/src/main/java/com/apoorvasingh2810/userauthenticationapp/LoginActivity.java
@@ -1,14 +1,28 @@
 package com.apoorvasingh2810.userauthenticationapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
     String TAG="LoginActivity";
+    FirebaseAuth mAuth;
+    EditText editTextEmail, editTextPassword;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +31,76 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         Log.d(TAG,"Started");
 
+        mAuth=FirebaseAuth.getInstance();
 
         findViewById(R.id.textViewSignup).setOnClickListener(this);
+        findViewById(R.id.buttonLogin).setOnClickListener(this);
+
+        editTextEmail=(EditText)findViewById(R.id.editTextEmail);
+        editTextPassword=(EditText)findViewById(R.id.editTextPassword);
+        progressBar=(ProgressBar)findViewById(R.id.progressbar);
+
+    }
+
+    private void userLogin() {
+
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Email is Required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+
+            editTextEmail.setError("Please enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editTextPassword.setError("Password is Required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(password.length()<6){
+            editTextPassword.setError("Minimum length of password should be 6");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        //Now all the validations are passed - so we can proceed for registration
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        progressBar.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent= new Intent(LoginActivity.this,ProfileActivity.class);
+
+                            //Protecting the user from going back to previous activity by pressing back button
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
 
     }
 
@@ -33,7 +115,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
                 break;
 
+            case R.id.buttonLogin:
+                userLogin();
+                break;
+
         }
 
     }
+
+
 }
--- app/src/main/java/com/apoorvasingh2810/userauthenticationapp/SignupActivity.java
+++ app/src/main/java/com/apoorvasingh2810/userauthenticationapp/SignupActivity.java
@@ -8,12 +8,14 @@ import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
 
 public class SignupActivity extends AppCompatActivity implements View.OnClickListener{
@@ -22,6 +24,7 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
     private FirebaseAuth mAuth;
 
     String TAG="Signup Activity";
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +33,13 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
 
         editTextEmail=(EditText)findViewById(R.id.editTextEmail);
         editTextPassword=(EditText)findViewById(R.id.editTextPassword);
+        progressBar=(ProgressBar)findViewById(R.id.progressbar);
 
         mAuth = FirebaseAuth.getInstance();
 
         findViewById(R.id.buttonSignUp).setOnClickListener(this);
+        findViewById(R.id.textViewLogin).setOnClickListener(this);
+
     }
 
     private void registerUser() {
@@ -66,27 +72,44 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
             return;
         }
 
+        progressBar.setVisibility(View.VISIBLE);
         //Now all the validations are passed - so we can proceed for registration
 
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        progressBar.setVisibility(View.GONE);
+
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
 //                            updateUI(user);
-                            Toast.makeText(SignupActivity.this ,"SUccessfully Registered",Toast.LENGTH_SHORT).show();
+                            Toast.makeText(SignupActivity.this ,"Successfully Registered",Toast.LENGTH_SHORT).show();
+
+                            Intent intent= new Intent(SignupActivity.this,ProfileActivity.class);
+
+                            //Protecting the user from going back to previous activity by pressing back button
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(SignupActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-//                            updateUI(null);
+
+                            //To check if user is already registered
+                            if(task.getException() instanceof FirebaseAuthUserCollisionException){
+                                Toast.makeText(SignupActivity.this, "You are already registered", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Toast.makeText(SignupActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+
+
                         }
 
-                        // ...
                     }
                 });
 
@@ -102,7 +125,7 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
                 break;
 
             case R.id.textViewLogin:
-                startActivity(new Intent(this,MainActivity.class));
+                startActivity(new Intent(this,LoginActivity.class));
                 break;
         }
 
